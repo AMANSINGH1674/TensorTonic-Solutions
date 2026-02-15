3b48e4c8b0f8a970c7a7a65eb27c1cba212784e0
AMAN SINGH <158809630+AMANSINGH1674@users.noreply.github.com>
2026-02-15 08:25:34 +0530

    Delete log-transform directory

--- log-transform/log-transform.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def log_transform(values):
-    """
-    Apply the log1p transformation: ln(1 + x).
-    """
-    # math.log1p is numerically more stable for very small values of x
-    return [math.log1p(v) for v in values]
